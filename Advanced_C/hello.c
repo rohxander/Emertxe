0d3bf0937cfd8845e8df7c4ab07ee54697f86049
rohxander <rohitmanjappallil@gmail.com>
2021-11-11 20:52:54 +0530

    11.11.2021

--- Advanced_C/hello.c
+++ Advanced_C/hello.c
@@ -4,8 +4,10 @@ int num1 = 3000;
 int num2 = 2000;
 int num3 = 1000;
 int result = 0;
-
-
+char ch='A';
+char bh= 65;
+int option = 2;
+float f = 4.1234567890;
 
 void main(){
 printf("Hello World\n");
@@ -14,9 +16,17 @@ printf("Size of short datatype is %lu\n",sizeof(short));
 printf("Size of long datatype is %lu\n",sizeof(long));
 printf("Size of double datatype is %lu\n",sizeof(double));
 printf("Size of long long datatype is %lu\n",sizeof(long long));
-printf("Size of short datatype is %lu\n",sizeof(short));
+printf("Size of long double int datatype is %lu\n",sizeof(long double));
 result = num1 + num2 + num3 ;
 printf("Total result = %d\n",result);
-
-
+printf("Integer : %d , Float : %f\n",option,f);
+float answer = option + f ;
+printf("Answer = %f\n",answer);
+printf("ASCI Decimal equivalent of A : %d\n",ch);
+printf("A : %c\n",ch);
+printf("character equivalent of 65 : %c\n",bh);
+// scanf("%d",&num1);
+// printf("%d",num1);
+scanf("%d%f%c\n",&num1,&f,&ch);
+printf("%d   %c   %f\n",num1,ch,f );
 }
