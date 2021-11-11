361488124512b92a75ac0dbbc2d34a3b9176e103
rohxander <rohitmanjappallil@gmail.com>
2021-11-11 16:23:47 +0530

    minor change

--- Advanced_C/hello.c
+++ Advanced_C/hello.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
 
+int num1 = 3000;
+int num2 = 2000;
+int num3 = 1000;
+int result = 0;
+
+
+
 void main(){
 printf("Hello World\n");
+printf("Size of char datatype is %lu\n",sizeof(char));
+printf("Size of short datatype is %lu\n",sizeof(short));
+printf("Size of long datatype is %lu\n",sizeof(long));
+printf("Size of double datatype is %lu\n",sizeof(double));
+printf("Size of long long datatype is %lu\n",sizeof(long long));
+printf("Size of short datatype is %lu\n",sizeof(short));
+result = num1 + num2 + num3 ;
+printf("Total result = %d\n",result);
+
+
 }
